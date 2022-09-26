9dfe562fe64b28cb236d93f28476fda4e56b0a81
Ruriko <47851127+Ruriko-Yuu@users.noreply.github.com>
2022-09-26 15:17:11 +0800

    ✨new vedio

--- src/pages/Newtab/index.jsx
+++ src/pages/Newtab/index.jsx
@@ -5,9 +5,11 @@ import Newtab from './Newtab';
 import './index.scss';
 
 render(<Newtab />, window.document.querySelector('#app-container'));
+const randomVal = Math.random()
 render(<video
-  src={Math.random() > 0.5 ? "../../../public/media/video/15v3-1080p.mp4" : "../../../public/media/video/bg.1080p.vp8.vorbis.webm"}
+  src={randomVal > 0.25 ? "../../../public/media/video/AL-yearV.mp4" : Math.random() > 0.5 ?  "../../../public/media/video/15v3-1080p.mp4" : "../../../public/media/video/bg.1080p.vp8.vorbis.webm"}
   autoPlay
+  playbackRate={randomVal > 0.25 ? 1 : 1}
   loop
 ></video>, window.document.querySelector('#bg-video'));
 

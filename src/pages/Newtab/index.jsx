ba4bcb2ef2e5cc1f4f262e081979d6d02d3da779
Ruriko <47851127+Ruriko-Yuu@users.noreply.github.com>
2022-11-21 13:47:08 +0800

    ✨莱莎联动视频

--- src/pages/Newtab/index.jsx
+++ src/pages/Newtab/index.jsx
@@ -7,7 +7,7 @@ import './index.scss';
 render(<Newtab />, window.document.querySelector('#app-container'));
 const randomVal = Math.random()
 render(<video
-  src={randomVal > 0.25 ? "../../../public/media/video/AL-yearV.mp4" : Math.random() > 0.5 ?  "../../../public/media/video/15v3-1080p.mp4" : "../../../public/media/video/bg.1080p.vp8.vorbis.webm"}
+  src={randomVal > 0.75 ? "../../../public/media/video/AL-yearV.mp4" : Math.random() > 0.25 ?  "../../../public/media/video/AL-lj.mp4" : "../../../public/media/video/bg.1080p.vp8.vorbis.webm"}
   autoPlay
   loop
 ></video>, window.document.querySelector('#bg-video'));
